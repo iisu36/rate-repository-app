b01383814db2ccef35a12a72b63e10343141e48e
Iisakki Kulhua <iisakki@kulhua.net>
2024-09-18 08:43:50 +0300

    10.24

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,10 +1,12 @@
 import { FlatList, View, StyleSheet, Pressable } from 'react-native'
+import { Searchbar } from 'react-native-paper'
 import RepositoryItem from './RepositoryItem'
 import { Picker } from '@react-native-picker/picker'
 import { useNavigate } from 'react-router-native'
 
 import useRepositories from '../hooks/useRepositories'
 import { useState } from 'react'
+import { useDebounce } from 'use-debounce'
 import theme from '../theme'
 
 const styles = StyleSheet.create({
@@ -15,7 +17,32 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />
 
-export const RepositoryListContainer = ({ repositories, pickerComponent }) => {
+const RepositoryList = () => {
+  const [sortingValue, setSortingValue] = useState('latest')
+  const [searchQuery, setSearchQuery] = useState('')
+  const [debouncedSearchQuery] = useDebounce(searchQuery, 500)
+  const { repositories } = useRepositories(
+    sorter(sortingValue)[0],
+    sorter(sortingValue)[1],
+    debouncedSearchQuery
+  )
+
+  return (
+    <RepositoryListContainer
+      repositories={repositories}
+      headerComponent={
+        <HeaderComponent
+          sortingValue={sortingValue}
+          setSortingValue={setSortingValue}
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
+      }
+    />
+  )
+}
+
+export const RepositoryListContainer = ({ repositories, headerComponent }) => {
   const navigate = useNavigate()
 
   const repositoryNodes = repositories
@@ -26,7 +53,7 @@ export const RepositoryListContainer = ({ repositories, pickerComponent }) => {
     <FlatList
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
-      ListHeaderComponent={pickerComponent}
+      ListHeaderComponent={headerComponent}
       renderItem={({ item }) => (
         <Pressable onPress={() => navigate(`${item.id}`)}>
           <RepositoryItem key={item.id} item={item}></RepositoryItem>
@@ -36,22 +63,40 @@ export const RepositoryListContainer = ({ repositories, pickerComponent }) => {
   )
 }
 
-const RepositoryList = () => {
-  const [sortingValue, setSortingValue] = useState(['CREATED_AT', 'DESC'])
-  const { repositories } = useRepositories(sortingValue[0], sortingValue[1])
-
+const HeaderComponent = ({
+  sortingValue,
+  setSortingValue,
+  setSearchQuery,
+  searchQuery,
+}) => {
   return (
-    <RepositoryListContainer
-      repositories={repositories}
-      pickerComponent={<PickerComponent setSortingValue={setSortingValue} />}
+    <>
+      <SearchBar
+        setSearchQuery={setSearchQuery}
+        searchQuery={searchQuery}
+      ></SearchBar>
+      <PickerComponent
+        sortingValue={sortingValue}
+        setSortingValue={setSortingValue}
+      ></PickerComponent>
+    </>
+  )
+}
+
+const SearchBar = ({ searchQuery, setSearchQuery }) => {
+  return (
+    <Searchbar
+      placeholder="Search"
+      onChangeText={setSearchQuery}
+      value={searchQuery}
     />
   )
 }
 
-const PickerComponent = ({ setSortingValue }) => {
+const PickerComponent = ({ sortingValue, setSortingValue }) => {
   return (
     <Picker
-      selectedValue="latest"
+      selectedValue={sortingValue}
       style={{
         height: 36,
         backgroundColor: 'inherit',
@@ -59,7 +104,7 @@ const PickerComponent = ({ setSortingValue }) => {
         fontSize: theme.fontSizes.subheading,
       }}
       onValueChange={(itemValue) => {
-        setSortingValue(sorter(itemValue))
+        setSortingValue(itemValue)
       }}
     >
       <Picker.Item label="Latest repositories" value="latest" />
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,12 +2,13 @@ import { useQuery } from '@apollo/client'
 
 import { GET_REPOSITORIES } from '../graphql/queries'
 
-const useRepositories = (orderBy, orderDirection) => {
+const useRepositories = (orderBy, orderDirection, searchKeyword) => {
   const { loading, data, refetch } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
     variables: {
       orderBy,
       orderDirection,
+      searchKeyword,
     },
   })
 
